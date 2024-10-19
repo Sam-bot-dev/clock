ba8158a8f6a23343d95c0127c059c0c1d3d223fd
Sammy <bavesh2006kumar@gmail.com>
2024-10-19 23:42:02 +0000

    grammer correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from tkinter import * # it is a basic grapic user interface (GUI) library in python
-import datetime # it is a module for manupilating date and time
+from tkinter import * # It is a basic grapic user interface (GUI) library in python
+import datetime # It is a module for manupilating date and time
 import time # This returns current time
 import winsound # This provides to windows basic sound playing capabilities
 
