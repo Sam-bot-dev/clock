9e08eeb0d7bca4ec3d180533f743abd6ddfe378f
Sammy <bavesh2006kumar@gmail.com>
2024-10-19 23:40:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import * # it is a basic grapic user interface (GUI) library in python
+import datetime # it is a module for manupilating date and time
+import time # This returns current time
+import winsound # This provides to windows basic sound playing capabilities
+
+# Create Object
+root = Tk()
