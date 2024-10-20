d430a1aae5cd1505fd124207d3081f174a1fb356
Rohan <rohanperla143@gmail.com>
2024-10-20 05:34:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import winsound # This provides to windows basic sound playing capabilities
 
 # Create Object
 root = Tk()
+
+ef alarm():
+    # Infinite Loop
+    while True:
+        # Set Alarm 
+        set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()}"
+ 
+        # Wait for one seconds
+        time.sleep(1)
+ 
+        # Get current time
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time,set_alarm_time)
+ 
+        # Check whether set alarm is equal to current time or not
+        if current_time == set_alarm_time:
+            print("Time to Wake up")
+            # Playing sound
+            winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
